ae9789fbd9d5479b5ff90a32dfd63ed26952be60
Martin Dubois <martindubois1602@gmail.com>
2022-04-07 16:48:37 +0200

    Refacto done

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,79 +9,98 @@ Guide.destroy_all
 
 Paladin = Job.create(name: "Paladin", role: "tank",
         level: "30", acronym: "PLD")
-        Paladin.image.attach(io: File.open(File.join(Rails.root, 'app/assets/images/Paladin.png')), filename: 'Paladin.png')
+        Paladin.icon.attach(io: File.open(File.join(Rails.root, 'app/assets/images/Paladin.png')), filename: 'Paladin.png')
+        Paladin.image.attach(io: File.open(File.join(Rails.root, 'app/assets/images/pld.png')), filename: 'pld.png')
 
 Warrior = Job.create(name: "Guerrier", role: "tank",
         level: "30", acronym: "WAR")
-        Warrior.image.attach(io: File.open(File.join(Rails.root, 'app/assets/images/Warrior.png')), filename: 'Warrior.png')
+        Warrior.icon.attach(io: File.open(File.join(Rails.root, 'app/assets/images/Warrior.png')), filename: 'Warrior.png')
+        Warrior.image.attach(io: File.open(File.join(Rails.root, 'app/assets/images/war.png')), filename: 'war.png')
 
 DarkKnight = Job.create(name: "Chevalier Noir", role: "tank",
         level: "30", acronym: "DRK")
-        DarkKnight.image.attach(io: File.open(File.join(Rails.root, 'app/assets/images/DarkKnight.png')), filename: 'DarkKnight.png')
+        DarkKnight.icon.attach(io: File.open(File.join(Rails.root, 'app/assets/images/DarkKnight.png')), filename: 'DarkKnight.png')
+        DarkKnight.image.attach(io: File.open(File.join(Rails.root, 'app/assets/images/drk.png')), filename: 'drk.png')
 
 Gunbreaker = Job.create(name: "Pistosabreur", role: "tank",
         level: "60", acronym: "GNB")
-        Gunbreaker.image.attach(io: File.open(File.join(Rails.root, 'app/assets/images/Gunbreaker.png')), filename: 'Gunbreaker.png')
+        Gunbreaker.icon.attach(io: File.open(File.join(Rails.root, 'app/assets/images/Gunbreaker.png')), filename: 'Gunbreaker.png')
+        Gunbreaker.image.attach(io: File.open(File.join(Rails.root, 'app/assets/images/gnb.png')), filename: 'gnb.png')
 
 WhiteMage = Job.create(name: "Mage Blanc", role: "healer",
         level: "30", acronym: "WHM")
-        WhiteMage.image.attach(io: File.open(File.join(Rails.root, 'app/assets/images/WhiteMage.png')), filename: 'WhiteMage.png')
+        WhiteMage.icon.attach(io: File.open(File.join(Rails.root, 'app/assets/images/WhiteMage.png')), filename: 'WhiteMage.png')
+        WhiteMage.image.attach(io: File.open(File.join(Rails.root, 'app/assets/images/whm.jpeg')), filename: 'whm.jpeg')
 
 Scholar = Job.create(name: "Erudit", role: "healer",
         level: "30", acronym: "SCH")
-        Scholar.image.attach(io: File.open(File.join(Rails.root, 'app/assets/images/Scholar.png')), filename: 'Scholar.png')
+        Scholar.icon.attach(io: File.open(File.join(Rails.root, 'app/assets/images/Scholar.png')), filename: 'Scholar.png')
+        Scholar.image.attach(io: File.open(File.join(Rails.root, 'app/assets/images/sch.png')), filename: 'sch.png')
 
 Astrologian = Job.create(name: "Astromancien", role: "healer",
         level: "30", acronym: "AST")
-        Astrologian.image.attach(io: File.open(File.join(Rails.root, 'app/assets/images/Astrologian.png')), filename: 'Astrologian.png')
+        Astrologian.icon.attach(io: File.open(File.join(Rails.root, 'app/assets/images/Astrologian.png')), filename: 'Astrologian.png')
+        Astrologian.image.attach(io: File.open(File.join(Rails.root, 'app/assets/images/ast.png')), filename: 'ast.png')
 
 Sage = Job.create(name: "Sage", role: "healer",
         level: "70", acronym: "SGE")
-        Sage.image.attach(io: File.open(File.join(Rails.root, 'app/assets/images/HealerRole.png')), filename: 'HealerRole.png')
+        Sage.icon.attach(io: File.open(File.join(Rails.root, 'app/assets/images/HealerRole.png')), filename: 'HealerRole.png')
+        Sage.image.attach(io: File.open(File.join(Rails.root, 'app/assets/images/sge.png')), filename: 'sge.png')
 
 Monk = Job.create(name: "Moine", role: "melee",
         level: "30", acronym: "MNK")
-        Monk.image.attach(io: File.open(File.join(Rails.root, 'app/assets/images/Monk.png')), filename: 'Monk.png')
+        Monk.icon.attach(io: File.open(File.join(Rails.root, 'app/assets/images/Monk.png')), filename: 'Monk.png')
+        Monk.image.attach(io: File.open(File.join(Rails.root, 'app/assets/images/mnk.png')), filename: 'mnk.png')
 
 Dragoon = Job.create(name: "Chevalier Dragon", role: "melee",
         level: "30", acronym: "DRG")
-        Dragoon.image.attach(io: File.open(File.join(Rails.root, 'app/assets/images/Dragoon.png')), filename: 'Dragoon.png')
+        Dragoon.icon.attach(io: File.open(File.join(Rails.root, 'app/assets/images/Dragoon.png')), filename: 'Dragoon.png')
+        Dragoon.image.attach(io: File.open(File.join(Rails.root, 'app/assets/images/drg.png')), filename: 'drg.png')
 
 Ninja = Job.create(name: "Ninja", role: "melee",
         level: "30", acronym: "NIN")
-        Ninja.image.attach(io: File.open(File.join(Rails.root, 'app/assets/images/Ninja.png')), filename: 'Ninja.png')
+        Ninja.icon.attach(io: File.open(File.join(Rails.root, 'app/assets/images/Ninja.png')), filename: 'Ninja.png')
+        Ninja.image.attach(io: File.open(File.join(Rails.root, 'app/assets/images/nin.png')), filename: 'nin.png')
 
 Samurai = Job.create(name: "Samouraï", role: "melee",
         level: "50", acronym: "SAM")
-        Samurai.image.attach(io: File.open(File.join(Rails.root, 'app/assets/images/Samurai.png')), filename: 'Samurai.png')
+        Samurai.icon.attach(io: File.open(File.join(Rails.root, 'app/assets/images/Samurai.png')), filename: 'Samurai.png')
+        Samurai.image.attach(io: File.open(File.join(Rails.root, 'app/assets/images/sam.png')), filename: 'sam.png')
 
 Reaper = Job.create(name: "Faucheur", role: "melee",
         level: "70", acronym: "RPR")
-        Reaper.image.attach(io: File.open(File.join(Rails.root, 'app/assets/images/DPSRole.png')), filename: 'DPSRole.png')
+        Reaper.icon.attach(io: File.open(File.join(Rails.root, 'app/assets/images/DPSRole.png')), filename: 'DPSRole.png')
+        Reaper.image.attach(io: File.open(File.join(Rails.root, 'app/assets/images/rpr.png')), filename: 'rpr.png')
 
 Bard = Job.create(name: "Barde", role: "distance",
         level: "30", acronym: "BRD")
-        Bard.image.attach(io: File.open(File.join(Rails.root, 'app/assets/images/Bard.png')), filename: 'Bard.png')
+        Bard.icon.attach(io: File.open(File.join(Rails.root, 'app/assets/images/Bard.png')), filename: 'Bard.png')
+        Bard.image.attach(io: File.open(File.join(Rails.root, 'app/assets/images/brd.png')), filename: 'brd.png')
 
 Machinist = Job.create(name: "Machiniste", role: "distance",
         level: "30", acronym: "MCH")
-        Machinist.image.attach(io: File.open(File.join(Rails.root, 'app/assets/images/Machinist.png')), filename: 'Machinist.png')
+        Machinist.icon.attach(io: File.open(File.join(Rails.root, 'app/assets/images/Machinist.png')), filename: 'Machinist.png')
+        Machinist.image.attach(io: File.open(File.join(Rails.root, 'app/assets/images/mch.png')), filename: 'mch.png')
 
 Dancer = Job.create(name: "Danseur", role: "distance",
         level: "60", acronym: "DNC")
-        Dancer.image.attach(io: File.open(File.join(Rails.root, 'app/assets/images/Dancer.png')), filename: 'Dancer.png')
+        Dancer.icon.attach(io: File.open(File.join(Rails.root, 'app/assets/images/Dancer.png')), filename: 'Dancer.png')
+        Dancer.image.attach(io: File.open(File.join(Rails.root, 'app/assets/images/dnc.png')), filename: 'dnc.png')
 
 BlackMage = Job.create(name: "Mage Noir", role: "caster",
         level: "30", acronym: "BLM")
-        BlackMage.image.attach(io: File.open(File.join(Rails.root, 'app/assets/images/BlackMage.png')), filename: 'BlackMage.png')
+        BlackMage.icon.attach(io: File.open(File.join(Rails.root, 'app/assets/images/BlackMage.png')), filename: 'BlackMage.png')
+        BlackMage.image.attach(io: File.open(File.join(Rails.root, 'app/assets/images/blm.png')), filename: 'blm.png')
 
 RedMage = Job.create(name: "Mage Rouge", role: "caster",
         level: "50", acronym: "RDM")
-        RedMage.image.attach(io: File.open(File.join(Rails.root, 'app/assets/images/RedMage.png')), filename: 'RedMage.png')
+        RedMage.icon.attach(io: File.open(File.join(Rails.root, 'app/assets/images/RedMage.png')), filename: 'RedMage.png')
+        RedMage.image.attach(io: File.open(File.join(Rails.root, 'app/assets/images/rdm.png')), filename: 'rdm.png')
 
 Summoner = Job.create(name: "Invocateur", role: "caster",
         level: "30", acronym: "SMN")
-        Summoner.image.attach(io: File.open(File.join(Rails.root, 'app/assets/images/Summoner.png')), filename: 'Summoner.png')
+        Summoner.icon.attach(io: File.open(File.join(Rails.root, 'app/assets/images/Summoner.png')), filename: 'Summoner.png')
+        Summoner.image.attach(io: File.open(File.join(Rails.root, 'app/assets/images/smn.jpeg')), filename: 'smn.jpeg')
 
 puts "Jobs créés"
 
