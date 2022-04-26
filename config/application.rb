88eb8741958791f25e043aeffac682e2bfbf7993
Martin DUBOIS <martindubois@gmail.com>
2022-04-26 16:21:18 +0200

    Fighting with Devise Gem

--- config/application.rb
+++ config/application.rb
@@ -19,7 +19,7 @@ require "sprockets/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module RailsFirstApp
+module OkZundu
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
@@ -28,7 +28,7 @@ module RailsFirstApp
     #
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
-    #
+    config.i18n.default_locale = :fr
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
     config.action_view.embed_authenticity_token_in_remote_forms = true
