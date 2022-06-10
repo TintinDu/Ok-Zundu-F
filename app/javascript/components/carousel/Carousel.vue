a2a6601e005b393d1082b6537739694b69e218e6
Martin Dubois <martindubois1602@gmail.com>
2022-06-10 18:01:25 +0200

    Components Created

---
+++ app/javascript/components/carousel/Carousel.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+  export default {
+    data () {
+      console.log(this.$children)
+      return {
+        index: 0,
+        slides: this.$children
+
+      }
+    }
+  }
+</script>
