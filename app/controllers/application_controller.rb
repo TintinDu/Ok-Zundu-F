89a9c0da1a76e86fa774fc41da5dfdbd7fabe95b
Martin Dubois <martindubois1602@gmail.com>
2022-07-29 12:56:41 +0200

    Deleting Devise 1st step

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,4 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!, :configure_permitted_parameters, if: :devise_controller?
 
-
-  def configure_permitted_parameters
-    # For additional fields in app/views/devise/registrations/new.html.erb
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:free_company, :avatar])
-
-    # For additional in app/views/devise/registrations/edit.html.erb
-    devise_parameter_sanitizer.permit(:account_update, keys: [:free_company, :avatar])
-  end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,5 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_one_attached :avatar
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise
 end
